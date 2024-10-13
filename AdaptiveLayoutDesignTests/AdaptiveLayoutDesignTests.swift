8670740113bed9283f378f8b93373617f86034fb
EQua-Dev <equality560@gmail.com>
2024-10-13 13:38:05 +0100

    Initial Commit

---
+++ AdaptiveLayoutDesignTests/AdaptiveLayoutDesignTests.swift
@@ -0,0 +1,17 @@
+//
+//  AdaptiveLayoutDesignTests.swift
+//  AdaptiveLayoutDesignTests
+//
+//  Created by Richard Uzor on 13/10/2024.
+//
+
+import Testing
+@testable import AdaptiveLayoutDesign
+
+struct AdaptiveLayoutDesignTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
