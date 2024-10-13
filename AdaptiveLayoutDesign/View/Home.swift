ef25153d148ac0c5e444f686152aafbdf3b2d259
EQua-Dev <equality560@gmail.com>
2024-10-13 17:09:44 +0100

    first commit

---
+++ AdaptiveLayoutDesign/View/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  AdaptiveLayoutDesign
+//
+//  Created by Richard Uzor on 13/10/2024.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Home()
+}
